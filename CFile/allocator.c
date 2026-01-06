7eb33c7bc1d453d7cdc82cff466190ccb26091c3
marginal <jay14454@gmail.com>
2026-01-06 23:27:29 +0800

    [新增]malloc功能

---
+++ CFile/allocator.c
@@ -0,0 +1,27 @@
+#include "../header/allocator.h"
+#include "../header/common.h"
+
+extern char __bss_end;
+
+static void* heap_ptr = NULL;
+
+void* SimpleAllocator(unsigned long size)
+{
+    if (heap_ptr == NULL)
+    {
+        heap_ptr = &__bss_end;
+    }
+
+    void* result_ptr = heap_ptr;
+
+    size = ALIGN8(size);
+
+    if ((unsigned long)result_ptr + size > MMIO_BASE)
+    {
+        return NULL;
+    }
+
+    heap_ptr = (void*)((unsigned long)result_ptr + size);
+
+    return result_ptr;
+}
