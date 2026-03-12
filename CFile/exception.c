dae047e01965b82eb28d831ef3c03981faa44b45
marginal <jay14454@gmail.com>
2026-03-12 21:43:01 +0800

    [修正]嘗試修正ASYNC UART SEND

--- CFile/exception.c
+++ CFile/exception.c
@@ -33,6 +33,53 @@ static inline unsigned long read_far_el1(void)
     return v;
 }
 
+static void irq_routing(unsigned long elr, unsigned long spsr, unsigned long *ctx)
+{
+    // IRQ 進來時，多半是 timer/uart/其他 device
+    mask_all_exceptions(); // 可選：避免印訊息時 nested
+
+    async_uart_puts("\n[EL0 IRQ]\n");
+    async_uart_puts("  ELR_EL1  = 0x"); 
+    async_uart_send_unsigned_long_integer(elr);  
+    async_uart_puts("\n");
+    async_uart_puts("  SPSR_EL1 = 0x"); 
+    async_uart_send_unsigned_long_integer(spsr); 
+    async_uart_puts("\n");
+
+    extern CtxT dtb_ctx;
+
+    // 讀取 Core 0 Interrupt Source
+    unsigned int irq_src = *((volatile unsigned int*)dtb_ctx.arm_local_interrupt);
+
+    // Bit 1 (值為 2) 代表 CNTPNSIRQ (Core Timer Interrupt)
+    if (irq_src == 2)
+    {
+        async_uart_puts("Core Timer Interrupt! Time: ");
+        get_timetick(); // 印出目前秒數
+        async_uart_puts("\n");
+
+        // Exercise 2 規定：下次 timeout 設為 2 秒後
+        set_core_timer_interrupt_second(2);
+    }
+    else if (irq_src == (1 << 8))
+    {
+        unsigned int uart_irq_pending = *((volatile unsigned int*)dtb_ctx.arm_ctrl_interrupt);
+        // 判斷是否為 AUX 中斷 (Bit 29)
+        if (uart_irq_pending & (1 << 29))
+        {
+            uart_interrupt_handler();
+        }
+    }
+    else
+    {
+        // 處理未知的中斷
+        async_uart_puts("\n[EL0 IRQ] Unknown IRQ source: 0x");
+        async_uart_send_hex(irq_src);
+        async_uart_puts("\n");
+    }
+    return;
+}
+
 void default_handler_dump_c(unsigned long esr, unsigned long elr, unsigned long spsr)
 {
     async_uart_puts("\n[DEFAULT EXCEPTION]\n");
@@ -99,48 +146,10 @@ void el0_sync_handler_c(unsigned long esr, unsigned long elr, unsigned long spsr
 
 void el0_irq_handler_c(unsigned long elr, unsigned long spsr, unsigned long *ctx)
 {
-    // IRQ 進來時，多半是 timer/uart/其他 device
-    mask_all_exceptions(); // 可選：避免印訊息時 nested
+    irq_routing(elr, spsr,ctx);
+}
 
-    async_uart_puts("\n[EL0 IRQ]\n");
-    async_uart_puts("  ELR_EL1  = 0x"); 
-    async_uart_send_unsigned_long_integer(elr);  
-    async_uart_puts("\n");
-    async_uart_puts("  SPSR_EL1 = 0x"); 
-    async_uart_send_unsigned_long_integer(spsr); 
-    async_uart_puts("\n");
-
-    extern CtxT dtb_ctx;
-
-    // 讀取 Core 0 Interrupt Source
-    unsigned int irq_src = *((volatile unsigned int*)dtb_ctx.arm_local_interrupt);
-
-    // Bit 1 (值為 2) 代表 CNTPNSIRQ (Core Timer Interrupt)
-    if (irq_src == 2)
-    {
-        async_uart_puts("Core Timer Interrupt! Time: ");
-        get_timetick(); // 印出目前秒數
-        async_uart_puts("\n");
-
-        // Exercise 2 規定：下次 timeout 設為 2 秒後
-        set_core_timer_interrupt_second(2);
-    }
-    else if (irq_src == (1 << 8))
-    {
-        unsigned int uart_irq_pending = *((volatile unsigned int*)dtb_ctx.arm_ctrl_interrupt);
-        // 判斷是否為 AUX 中斷 (Bit 29)
-        if (uart_irq_pending & (1 << 29))
-        {
-            uart_interrupt_handler();
-        }
-    }
-    else
-    {
-        // 處理未知的中斷
-        async_uart_puts("\n[EL0 IRQ] Unknown IRQ source: 0x");
-        async_uart_send_hex(irq_src);
-        async_uart_puts("\n");
-    }
-    return;
-
+void el1_irq_handler_c(unsigned long elr, unsigned long spsr, unsigned long *ctx)
+{
+    irq_routing(elr, spsr, ctx);
 }
